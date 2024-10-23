9150a599e5c08423e8dadd6437ba386e41d9e80e
Ahmed Khaled <ahmed@mrkhalednasr.com>
2024-10-23 16:32:23 +0300

    feat: single blog view

--- personal-blog-frontend/src/app/ui/user/pages/single-blog/single-blog.component.ts
+++ personal-blog-frontend/src/app/ui/user/pages/single-blog/single-blog.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { BlogService } from '../../../../services/blog-service/blog.service';
+import { Observable } from 'rxjs';
+import { Blog } from '../../../../services/blog-service/blog.model';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-single-blog',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './single-blog.component.html',
-  styleUrl: './single-blog.component.scss'
+  styleUrl: './single-blog.component.scss',
 })
 export class SingleBlogComponent {
-
+  blog$!: Observable<Blog>;
+  constructor(private blogService: BlogService, private route: ActivatedRoute) {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.blog$ = this.blogService.getBlogById(id);
+    }
+  }
 }
